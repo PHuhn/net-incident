fdaeff549c763faeb097b16f9f7232ddab1d9aa1
PHuhn <PhilHuhn@yahoo.com>
2018-05-25 15:58:41 -0400

    Converting from spaces to tab indentation.

--- net-incident/src/app/public/about/about.component.ts
+++ net-incident/src/app/public/about/about.component.ts
@@ -2,21 +2,21 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+	selector: 'app-about',
+	templateUrl: './about.component.html',
+	styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-  //
-  applicationName = 'Network Incident';
-  companyName = 'Northern Software Group';
-  copyright ='Copyright © 2018';
-  version ='Alpha';
-  //
-  constructor() { }
-  //
-  ngOnInit() {
-  }
-  //
+	//
+	applicationName = 'Network Incident';
+	companyName = 'Northern Software Group';
+	copyright ='Copyright © 2018';
+	version ='Alpha';
+	//
+	constructor() { }
+	//
+	ngOnInit() {
+	}
+	//
 }
 // ===========================================================================
--- net-incident/src/app/public/contact/contact.component.ts
+++ net-incident/src/app/public/contact/contact.component.ts
@@ -2,22 +2,22 @@
 import { Component, OnInit } from '@angular/core';
 //
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+	selector: 'app-contact',
+	templateUrl: './contact.component.html',
+	styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-  //
-  applicationName = 'Network Incident';
-  companyName = 'Northern Software Group';
-  contactName ='Phil Huhn';
-  contactEmail ='PhilHuhn@yahoo.com';
-  githubLink = 'https://github.com/PHuhn?tab=repositories';
-  //
-  constructor() { }
-  //
-  ngOnInit() {
-  }
-  //
+	//
+	applicationName = 'Network Incident';
+	companyName = 'Northern Software Group';
+	contactName ='Phil Huhn';
+	contactEmail ='PhilHuhn@yahoo.com';
+	githubLink = 'https://github.com/PHuhn?tab=repositories';
+	//
+	constructor() { }
+	//
+	ngOnInit() {
+	}
+	//
 }
 // ===========================================================================
