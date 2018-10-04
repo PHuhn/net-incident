a84221e686daeecdda671820b9bab75f32bbcc79
PHuhn <PhilHuhn@yahoo.com>
2018-10-03 21:53:13 -0400

    Cleanup modules imports in app.module.ts

---
+++ net-incident/src/app/APP.MODULE-PRIMENG.ts
@@ -0,0 +1,14 @@
+//
+import { TableModule } from 'primeng/components/table/table';
+import { DialogModule } from 'primeng/components/dialog/dialog';
+import { ConfirmDialogModule } from 'primeng/components/confirmdialog/confirmdialog';
+import { DropdownModule } from 'primeng/components/dropdown/dropdown';
+import { MenubarModule } from 'primeng/components/menubar/menubar';
+//
+export const APP_MODULE_PRIMENG = [
+	TableModule,
+	DialogModule,
+	ConfirmDialogModule,
+	DropdownModule,
+	MenubarModule
+];
--- net-incident/src/app/app.module.ts
+++ net-incident/src/app/app.module.ts
@@ -1,16 +1,9 @@
 // File: app.module.ts
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-//
-import { TableModule } from '../../node_modules/primeng/components/table/table';
-import { DialogModule } from '../../node_modules/primeng/components/dialog/dialog';
-import { ConfirmDialogModule } from '../../node_modules/primeng/components/confirmdialog/confirmdialog';
-import { DropdownModule } from '../../node_modules/primeng/components/dropdown/dropdown';
-import { MenubarModule } from '../../node_modules/primeng/components/menubar/menubar';
 //
 import { APP_PROVIDERS } from './APP.PROVIDERS';
 import { APP_COMPONENTS } from './APP.COMPONENTS';
+import { APP_MODULE_PRIMENG } from './APP.MODULE-PRIMENG';
 import { AppRoutingModule } from './app-routing.module';
 import { GlobalModule } from './global/global.module';
 import { AppComponent } from './app.component';
@@ -23,11 +16,7 @@ import { AppComponent } from './app.component';
 	imports: [
 		AppRoutingModule,
 		GlobalModule,
-		TableModule,
-		DialogModule,
-		ConfirmDialogModule,
-		DropdownModule,
-		MenubarModule
+		APP_MODULE_PRIMENG
 	],
 	providers: [
 		APP_PROVIDERS
