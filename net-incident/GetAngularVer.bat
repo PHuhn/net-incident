34441be8610f97d63fc4c97e35f8a560040fb062
PHuhn <PhilHuhn@yahoo.com>
2019-12-10 10:53:10 -0500

    Using placeholder

--- net-incident/GetAngularVer.bat
+++ net-incident/GetAngularVer.bat
@@ -1,4 +1,5 @@
 @REM Note VER is reserved word
-@SET VRSN=7.2.15
+@SET VRSN=8.2.14
 npm install --save-dev @angular/animations@%VRSN% @angular/common@%VRSN% @angular/compiler@%VRSN% @angular/compiler-cli@%VRSN% @angular/core@%VRSN% @angular/forms@%VRSN% @angular/language-service@%VRSN% @angular/platform-browser@%VRSN% @angular/platform-browser-dynamic@%VRSN% @angular/platform-server@%VRSN% @angular/router@%VRSN%
 npm install --save-dev @angular-devkit/build-angular@latest @angular/cli@latest
+pause
