700002e04c8ae3fc862c1e07676207ac309b0474
PHuhn <PhilHuhn@yahoo.com>
2021-08-31 16:50:14 -0400

    audit fix

--- net-incident/src/app/public/about/about.component.ts
+++ net-incident/src/app/public/about/about.component.ts
@@ -12,7 +12,7 @@ export class AboutComponent {
 	copyright ='Copyright © 2021';
 	// (angular version).major.minor.build
 	ng_version ='12';
-	version ='12.1.0.35';
+	version = `${this.ng_version}.1.0.35`;
 	//
 	constructor() { }
 	//
